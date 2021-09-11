82ffc5d7014a142d61831d6e3b33decd8f59fa5b
Matija Osrečki <matija.osrecki@gmail.com>
2021-09-11 14:33:38 +0200

    fixing shit, losing my mind with forms

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const isUrl = require('validator/lib/isURL');
 const shorturlRouter = require('./router/shorturl.js');
 
 const app = express();
@@ -11,6 +12,10 @@ app.use(express.json());
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 app.use(shorturlRouter);
+
+app.get('/', (req, res) => {
+	res.sendFile(`${__dirname}/views/index.html`);
+});
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
